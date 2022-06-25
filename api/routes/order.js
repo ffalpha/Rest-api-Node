1811a2678fd20143f833678bde3a5dc7fa16cd6a
kalana.mihiranga <kalana.mihiranga@pickme.lk>
2022-06-25 15:19:26 +0530

    Added  error handeling

--- api/routes/order.js
+++ api/routes/order.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-
+//Hanfle incoming GET request
 router.get('/', (req, res, next) => {
     res.status(200).json({
         message: "Orders were feteched"
--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 const express=require('express');
 const app=express();
+const morgan=require('morgan') //for loging
 const productsRoutes=require('./api/routes/product')
-const orderRoutes=require('./api/routes/order')
+const orderRoutes=require('./api/routes/order');
+const e = require('express');
 
+app.use(morgan('dev'));
 app.use('/products',productsRoutes);
 app.use('/orders',orderRoutes);
 
+//for handling no routes
+app.use((req,res,next)=>{
+    const error=new Error('Not found');
+    error.stauts=404;
+    next(error); //forward to following fucntions
+})
 
+
+//handle all errors
+app.use((error,req,res,next)=>{
+    res.stauts=error.stauts || 500 ;
+    res.json({
+        error:{
+            message:error.message
+        }
+    })
+})
 module.exports=app;
 
 
